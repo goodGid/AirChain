a656a32ba8355c0b1217c9b568c45a40fb758faa
Kiyong <gy8971@gmail.com>
2018-06-06 16:50:03 +0900

    Update Pool.js File

--- module/pool.js
+++ module/pool.js
@@ -6,23 +6,6 @@ const pool = require('../config/dbPool.js');
  Modularize DB Connection
 */
 module.exports = {
-  queryParamCnt_None : async (...args) => {
-    const query = args[0];
-    let result;
-    try {
-      var connection = await pool.getConnection();
-      result = await connection.query(query) || null;
-    }
-    catch(err) {
-      console.log("mysql error! err log =>" + err);
-      next(err);
-    }
-    finally {
-      pool.releaseConnection(connection);
-
-      return result;
-    }
-  },
   Query : async (...args) => {
     const query = args[0];
     const data = args[1];
